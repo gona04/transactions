6d25806404589a928b4723b99da3e14545faae0e
Work Goonja <workgoonja@goonjas-MacBook-Pro.local>
2024-07-15 08:59:10 -0400

    backend will send all the data filtering will be done by front-end

--- back-end/controller/transaction-controller.js
+++ back-end/controller/transaction-controller.js
@@ -3,11 +3,8 @@ const Transaction = require('../models/transaction');
 const transactionController = {
   getAllTransactions: async (req, res) => {
     try {
-      // Fetch transactions that are either COMPLETED, IN PROGRESS, or REJECTED
-      // and sort them by date in descending order (most recent first)
-      const transactions = await Transaction.find({
-        status: { $in: ['COMPLETED', 'IN PROGRESS', 'REJECTED'] }
-      }).sort({ date: -1 });
+      // Fetch all transactions and sort them by date in descending order (most recent first)
+      const transactions = await Transaction.find({}).sort({ date: -1 });
 
       res.status(200).json(transactions);
     } catch (error) {
@@ -18,14 +15,11 @@ const transactionController = {
   getTransactionById: async (req, res) => {
     const { id } = req.params;
     try {
-      // Attempt to find a transaction by ID that also matches one of the specified statuses
-      const transaction = await Transaction.findOne({
-        _id: id,
-        status: { $in: ['COMPLETED', 'IN PROGRESS', 'REJECTED'] }
-      });
+      // Attempt to find a transaction by ID without filtering by status
+      const transaction = await Transaction.findById(id);
   
       if (!transaction) {
-        return res.status(404).send(`Transaction with ID: ${id} not found or status not matching.`);
+        return res.status(404).send(`Transaction with ID: ${id} not found.`);
       }
   
       res.status(200).json(transaction);
